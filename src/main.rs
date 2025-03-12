21c51314f50c36d2e79232c2335ad4b8cb0f98a2
Pedro Manse <pedro.manse@dmk3.com.br>
2025-03-12 16:53:19 -0300

    pest grammar

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use pest::iterators::Pair;
+use pest::Parser;
+
+#[derive(pest_derive::Parser)]
+#[grammar = "../trr.pest"] // relative to src
+struct MyParser;
+
 fn main() {
-    println!("Hello, world!");
+    let a = MyParser::parse(Rule::code, r#"
+let noop ret func = :{
+    b.print("hello");
+    return this;
+};
+let z = noop ret func(3);
+"#.trim()).unwrap();
+    let mut pairs: Vec<Pair<_>> = a.collect();
+    while let Some(p) = pairs.pop() {
+        println!("{p:?}");
+        
+        pairs.append(&mut p.into_inner().collect::<Vec<Pair<_>>>());
+    }
 }
